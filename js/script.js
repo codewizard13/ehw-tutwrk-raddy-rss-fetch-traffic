c091e2bff9b9f4ed173789c71a8c081ed7e4118e
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-27 16:41:39 -0500

    UPDATE index.html:  00:18:50 - RSS feed console.log() with DOMParser()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // script.js | Eric Hepperle
+
+loadXMLFeed = () => {
+  const url = "docs/rss-va-traffic-all.xml"
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      let parser = new DOMParser()
+      let xml = parser.parseFromString(data, "application/xml")
+      console.log(xml)
+    })
+}
+
+loadXMLFeed()
