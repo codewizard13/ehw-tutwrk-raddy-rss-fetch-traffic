488c431bb00af5113a4916f90820745aadbdb402
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-27 16:56:44 -0500

    UPDATE index.html:  00:25:38 - Populate list with dummy text and counter in FOR loop

--- js/script.js
+++ js/script.js
@@ -14,5 +14,19 @@ loadXMLFeed = () => {
 document.addEventListener("DOMContentLoaded", loadXMLFeed)
 
 function displayTrafficList(x) {
-  console.log(x)
+
+  let list = document.getElementById('item')
+  let item = x.getElementsByTagName('item')
+  let itemNum = x.getElementsByTagName('item').length
+  console.log(itemNum)
+
+  for (let i = 0; i < itemNum; i++) {
+    let li = document.createElement('li')
+    li.className = "listItem"
+
+    li.innerHTML = `${i + 1} RSS Feed`
+
+    list.appendChild(li)
+  }
+
 }
