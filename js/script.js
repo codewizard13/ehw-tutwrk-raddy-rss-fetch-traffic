2b19785d4f039e2e4fb91e2e854ca5b80671515d
Eric L. Hepperle <codewizard13@gmail.com>
2022-06-28 05:00:23 -0500

    UPDATE script.js:  00:27:54 - DISPLAY titles correctly as H3 list

--- js/script.js
+++ js/script.js
@@ -26,18 +26,20 @@ function displayTrafficList(x) {
 
     let thisItem = item[i].getElementsByTagName('title')[0]
 
-    li.innerHTML =
-      `
-    <h3>${item[i].getElementsByTagName('title')[0]}</h3>
-        `
-
-    list.appendChild(li)
     // using `wholeText` will allow us to read CDATA when it's located in
     //  a new line, as per:
     // https://stackoverflow.com/questions/1736122/read-cdata-in-xml-from-javascript
     const cdataValue = thisItem?.firstChild?.wholeText?.trim();
     console.log(item[i].getElementsByTagName('title')[0])
     console.log(cdataValue)
+
+    li.innerHTML =
+      `
+    <h3>${cdataValue}</h3>
+        `
+
+    list.appendChild(li)
+
   }
 
 }
